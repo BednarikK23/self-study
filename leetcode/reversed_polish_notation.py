951c2ce4023d1ace8d85054d074b257097fc3c96
BednarikK23 <bednarik2387@gmail.com>
2023-06-28 16:40:46 +0200

    added links to my solutions on leetcode

--- leetcode/reversed_polish_notation.py
+++ leetcode/reversed_polish_notation.py
@@ -1,3 +1,4 @@
+# https://leetcode.com/problems/evaluate-reverse-polish-notation/submissions/981616435/
 # You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
 #
 # Evaluate the expression. Return an integer that represents the value of the expression.
