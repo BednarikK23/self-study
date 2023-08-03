166db411b71cb539c790c3e83eb920f587cdfae5
BednarikK23 <bednarik2387@gmail.com>
2023-08-03 15:02:54 +0200

    movig base

---
+++ flappy_bird_ai/base.py
@@ -0,0 +1,25 @@
+from consts import BASE_IMG, OBJ_VELOCITY
+
+
+class Base:
+    VEL = OBJ_VELOCITY
+    WIDTH = BASE_IMG.get_width()
+    IMG = BASE_IMG
+
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.WIDTH
+
+    def move(self):
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+
+    def draw(self, win):
+        win.blit(self.IMG, (self.x1, self.y))
+        win.blit(self.IMG, (self.x2, self.y))
