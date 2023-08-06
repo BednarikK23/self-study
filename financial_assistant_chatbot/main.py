1c546f61c179a06fb032700831bf4638837139aa
BednarikK23 <bednarik2387@gmail.com>
2023-08-06 11:42:11 +0200

    starting new project on fince chatbot

---
+++ financial_assistant_chatbot/main.py
@@ -0,0 +1,19 @@
+import json
+import openai
+import matplotlib.pyplot as plt
+import pandas as pd
+import yfinance as yf
+import streamlit as st
+
+
+
+
+
+def main():
+    openai.api_key = open('API_KEY', 'r').read()
+
+    return
+
+
+if __name__ == '__main__':
+    main()
