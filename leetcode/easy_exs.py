11a1e3e257e0258b89b59e4f3bbbc6f3757c015b
BednarikK23 <bednarik2387@gmail.com>
2023-08-20 02:49:11 +0200

    roman to int

--- leetcode/easy_exs.py
+++ leetcode/easy_exs.py
@@ -57,3 +57,25 @@ class Solution:
             old //= 10
 
         return old == new or old == new // 10
+
+    numbers = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+
+    def romanToInt(self, s: str) -> int:
+        # https://leetcode.com/problems/roman-to-integer/submissions/1026135692/
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+
+        res = 0
+        for c in s:
+            res += self.numbers[c]
+
+        return res
