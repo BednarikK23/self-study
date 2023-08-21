a2b406bf4f4e2f2611f02888f68fc6670e836c02
BednarikK23 <bednarik2387@gmail.com>
2023-08-22 00:01:57 +0200

    dont have tutorial on go done but tried one easy in this lang

--- leetcode/easies/easy.go
+++ leetcode/easies/easy.go
@@ -1,15 +1,19 @@
+package main
+
 func climbStairs(n int) int {
-    // https://leetcode.com/problems/climbing-stairs/submissions/1027783423/
-    if n == 1 {return 1}
+	// https://leetcode.com/problems/climbing-stairs/submissions/1027783423/
+	if n == 1 {
+		return 1
+	}
 
-    // bottom up dp - ways to get to top for first 6: [8, 5, 3, 2, 1, 1]
-    lst, pre := 1, 1
+	// bottom up dp - ways to get to top for first 6: [8, 5, 3, 2, 1, 1]
+	lst, pre := 1, 1
 
-    for i := 0; i < n - 1; i ++ {
-        tmp := lst
-        lst = lst + pre
-        pre = tmp
-    }
+	for i := 0; i < n-1; i++ {
+		tmp := lst
+		lst = lst + pre
+		pre = tmp
+	}
 
-    return lst
+	return lst
 }
