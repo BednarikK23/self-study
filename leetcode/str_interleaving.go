f2ad727f422a931269a7ff167c8a0aaecac8b8a4
BednarikK23 <bednarik2387@gmail.com>
2023-08-25 23:54:33 +0200

    string interleaving

--- leetcode/str_interleaving.go
+++ leetcode/str_interleaving.go
@@ -1,22 +1,27 @@
-package leetcode
+package main
+
+// https://leetcode.com/problems/interleaving-string/submissions/1031788494/
 
 func isInterleave(s1 string, s2 string, s3 string) bool {
-	l1, l2 := len(s1), len(s2),
-	if l1 + l2 != len(s3) { return false }
+	l1, l2 := len(s1), len(s2)
+	if l1+l2 != len(s3) {
+		return false
+	}
 
-	dp := make([]bool, l2 + 1)
-	dp[0] = true
+	res := make([]bool, l2+1)
+	res[0] = true
 
 	for j := 1; j <= l2; j++ {
-		dp[j] = dp[j-1] && s2[j-1] == s3[j-1]
+		res[j] = res[j-1] && s2[j-1] == s3[j-1]
 	}
 
 	for i := 1; i <= l1; i++ {
-		dp[0] = dp[0] && s1[i-1] == s3[i-1]
+		res[0] = res[0] && s1[i-1] == s3[i-1]
+
 		for j := 1; j <= l2; j++ {
-			dp[j] = (dp[j] && s1[i-1] == s3[i+j-1]) || (dp[j-1] && s2[j-1] == s3[i+j-1])
+			res[j] = (res[j] && s1[i-1] == s3[i+j-1]) || (res[j-1] && s2[j-1] == s3[i+j-1])
 		}
 	}
 
-	return dp[l2]
+	return res[l2]
 }
