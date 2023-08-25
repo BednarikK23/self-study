2d6f9b0997cddb27e194b3e4ec763b7786a83df7
BednarikK23 <bednarik2387@gmail.com>
2023-08-25 23:51:14 +0200

    string interleaving

---
+++ leetcode/str_interleaving.go
@@ -0,0 +1,22 @@
+package leetcode
+
+func isInterleave(s1 string, s2 string, s3 string) bool {
+	l1, l2 := len(s1), len(s2),
+	if l1 + l2 != len(s3) { return false }
+
+	dp := make([]bool, l2 + 1)
+	dp[0] = true
+
+	for j := 1; j <= l2; j++ {
+		dp[j] = dp[j-1] && s2[j-1] == s3[j-1]
+	}
+
+	for i := 1; i <= l1; i++ {
+		dp[0] = dp[0] && s1[i-1] == s3[i-1]
+		for j := 1; j <= l2; j++ {
+			dp[j] = (dp[j] && s1[i-1] == s3[i+j-1]) || (dp[j-1] && s2[j-1] == s3[i+j-1])
+		}
+	}
+
+	return dp[l2]
+}
