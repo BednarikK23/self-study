308a14c9bf04b13273c23446107ee13d0b3fcc2d
BednarikK23 <bednarik2387@gmail.com>
2023-09-04 23:06:48 +0200

    delete duplicates from ll added

--- leetcode/easies/easy6.py
+++ leetcode/easies/easy6.py
@@ -35,3 +35,18 @@ class Solution:
         """
         mem = {0: 0, 1: 1}
         return [self.rec(i, mem) for i in range(n + 1)]
+
+    # https://leetcode.com/problems/remove-duplicates-from-sorted-list/submissions/1040684238/
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None:
+            return None
+
+        prev, node = head, head.next
+        while node is not None:
+            if prev.val == node.val:
+                prev.next = node.next
+            else:
+                prev = node
+            node = node.next
+
+        return head
